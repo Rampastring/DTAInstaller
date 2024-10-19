2d570b54b8596dba910a4b4b3a090d527de29cc2
Rampastring <rampastring@outlook.com>
2024-10-19 17:18:13 +0300

    Update installer contents

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Rampastring")]
 [assembly: AssemblyProduct("Dawn of the Tiberium Age")]
-[assembly: AssemblyCopyright("Copyright © Rampastring 2017")]
+[assembly: AssemblyCopyright("Copyright © Rampastring 2017-2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.17.0.1")]
-[assembly: AssemblyFileVersion("1.17.0.1")]
+[assembly: AssemblyVersion("13.1.1.0")]
+[assembly: AssemblyFileVersion("13.1.1.0")]
